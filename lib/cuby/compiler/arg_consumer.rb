5539727d7c9f1a7cfc92eb0abacf529d187a6027
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-07-26 15:59:47 +0530

    cuby: compiler[arg_consumer]

---
+++ lib/cuby/compiler/arg_consumer.rb
@@ -0,0 +1,33 @@
+module Cuby 
+    class Compiler
+        class ArgConsumer
+            def initialize
+                @from_side = :left
+            end
+
+            attr_reader :from_side
+
+            def consume(exp)
+                raise 'do not pass an Array to ArgConsumer' if exp.instance_of?(Array)
+                args = exp.lefts + [exp.rest].compact + exp.rights
+                while args.any?
+                    if @from_side == :left
+                        arg = args.shift
+                    else 
+                        arg = args.pop
+                    end
+
+                    if yield(arg) == :reverse
+                        reverse
+                    end
+                end
+            end
+
+            private 
+
+            def reverse
+                @from_side = { left: :right, right: :left }.fetch(@from_side)
+            end
+        end
+    end
+end
