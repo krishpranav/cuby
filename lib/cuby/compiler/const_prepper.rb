8a433d802ba62cd1ed446e63fedf50c8e2a5126d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-07-30 17:49:41 +0530

    compiler: const_prepper

---
+++ lib/cuby/compiler/const_prepper.rb
@@ -0,0 +1,31 @@
+module Cuby
+    class Compiler
+      class ConstPrepper
+        def initialize(node, pass:)
+          @private = true
+          case node.type
+          when :constant_read_node, :constant_target_node
+            @name = node.name
+            @namespace = PushSelfInstruction.new
+          when :constant_path_node, :constant_path_target_node
+            raise 'unexpected child here' if node.name.nil?
+            @name = node.name
+            if node.parent
+              @namespace = pass.transform_expression(node.parent, used: true)
+              @private = false
+            else
+              @namespace = PushObjectClassInstruction.new
+            end
+          else
+            raise "Unknown constant name type #{node.inspect}"
+          end
+        end
+  
+        attr_reader :name, :namespace
+  
+        def private?
+          @private
+        end
+      end
+    end
+  end
